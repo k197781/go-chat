5f1b1245b7a9c30fe337f379dc56678fc697ba59
ItohKaori <k197781@kansai-u.ac.jp>
2019-04-16 02:58:29 +0900

    add room model

--- room.go
+++ room.go
@@ -1,6 +1,36 @@
 package main
 
 type room struct {
-	// message sent to other clients is added to forward field.
+	// message sent to other clients is added to forward field
 	forward chan []byte
+	// channel for user will join is added to join filed
+	join chan *client
+	// channel for user will join is added to join filed
+	leave chan *client
+	// all active clients is added to clients field
+	clients map[*client]bool
+}
+
+// goroutine is perfomed in the background
+func (r *room) run() {
+	for {
+		select {
+		case client := <-r.join:
+			r.clients[client] = true
+		case client := <-r.leave:
+			delete(r.clients, client)
+			close(client.send)
+		case msg := <-r.forward:
+			// send massage for all clients
+			for client := range r.clients {
+				select {
+				case client.send <- msg:
+				default:
+					// if a failure occurs
+					delete(r.clients, client)
+					close(client.send)
+				}
+			}
+		}
+	}
 }
