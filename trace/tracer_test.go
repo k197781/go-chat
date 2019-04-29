c59129587b710b83890e9e8720c2cdaee5551bea
ItohKaori <k197781@kansai-u.ac.jp>
2019-04-30 04:02:58 +0900

    change method position

--- trace/tracer_test.go
+++ trace/tracer_test.go
@@ -2,23 +2,8 @@ package trace
 
 import (
 	"bytes"
-	"fmt"
-	"io"
 	"testing"
 )
-
-type tracer struct {
-	out io.Writer
-}
-
-func (t *tracer) Trace(a ...interface{}) {
-	t.out.Write([]byte(fmt.Sprint(a...)))
-	t.out.Write([]byte("\n"))
-}
-
-func New(w io.Writer) Tracer {
-	return &tracer{out: w}
-}
 
 func TestNew(t *testing.T) {
 	var buf bytes.Buffer
